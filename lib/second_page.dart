b1c3fe830a5e83ad328f34d533180faffb9de330
KIRAN M R <iamthekiranmr@gmail.com>
2021-11-03 13:11:16 +0530

    options randomised

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:trivia_app/api_trivia.dart';
 import 'package:trivia_app/model/questions.dart';
+import 'dart:math';
 
 class Secondpage extends StatefulWidget {
   const Secondpage({Key? key}) : super(key: key);
@@ -62,10 +63,39 @@ class _SecondpageState extends State<Secondpage> {
 }
 
 _buttons(results,index){
-  return Column(children: [ ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[0]}  ')),
-    ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[1]}  ')),
-    ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[2]}  ')),
-    ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).correct_answer}  '))],);
+  Random random = new Random();
+  int a = random.nextInt(4);
+  int b = a + 1;
+  int c = a + 2;
+  int d = a + 3;
+  if (b == 4 && c == 5 && d== 6){
+    b = 0;
+    c = 1;
+    d = 2;
+  }else if (c == 4 && d== 5){
+    c = 0;
+    d = 1;
+  }else if (d == 4){
+    d = 0;
+  }
+  print(a);
+  return Column(children:
+  [_options(a,results,index),
+    _options(b,results,index),
+    _options(c,results,index),
+    _options(d,results,index)],);
 
 }
 
+_options(int a,results,index){
+  if (a==0){
+    return ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).correct_answer}  '));
+  }else if(a == 1) {
+    return ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[0]}  '));
+  }else if(a == 2) {
+    return ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[1]}  '));
+  }else if(a == 3) {
+    return ElevatedButton(onPressed: (){}, child: Text('  ${results.elementAt(index).incorrect_answers[2]}  '));
+  }
+
+}
